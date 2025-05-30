2a5c06287475c1f383561bc7a24de1f3db9b15d4
qalbeali <qalbeali8@gmail.com>
2025-05-30 17:53:24 +0500

    Blog Publishing

--- src/components/blocks/BlogBlock/nodes/FeatureImageBlock.ts
+++ src/components/blocks/BlogBlock/nodes/FeatureImageBlock.ts
@@ -1,89 +1,93 @@
 import { Node, mergeAttributes } from "@tiptap/core";
 import { ReactNodeViewRenderer } from "@tiptap/react";
+import { Fragment } from "@tiptap/pm/model";
 import { FeatureImageBlockNodeView } from "./FeatureImageBlockNodeView";
 
+export const FeatureImageBlock = Node.create({
+  name: "featureImageBlock",
+  group: "block",
+  content: "altBlock captionBlock",
+  draggable: true,
+  isolating: true,
+  atom: false,
+
+  addAttributes() {
+    return {
+      src: {
+        default: "",
+      },
+    };
+  },
+
+  parseHTML() {
+    return [
+      {
+        tag: "feature-image-block",
+        getContent: (node, schema) => {
+          const element = node as HTMLElement;
+          const imgElement = element.querySelector("img");
+          const src = imgElement?.getAttribute("src") || "";
+          const altElement = element.querySelector(".alt-block");
+          const captionElement = element.querySelector(".caption-block");
+          const altText = altElement?.textContent || "";
+          const captionText = captionElement?.textContent || "";
+
+          return Fragment.fromArray([
+            schema.nodes.altBlock.create({}, [schema.text(altText)]),
+            schema.nodes.captionBlock.create({}, [schema.text(captionText)]),
+          ]);
+        },
+      },
+    ];
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    return ["feature-image-block", mergeAttributes(HTMLAttributes), 0];
+  },
+
+  addNodeView() {
+    return ReactNodeViewRenderer(FeatureImageBlockNodeView, {
+      stopEvent: () => false,
+    });
+  },
+});
+
+const AltBlock = Node.create({
+  name: "altBlock",
+  group: "block",
+  content: "inline*",
+  defining: true,
+
+  parseHTML() {
+    return [{ tag: "div.alt-block" }];
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    return ["div", mergeAttributes(HTMLAttributes, { class: "alt-block" }), 0];
+  },
+});
+
+const CaptionBlock = Node.create({
+  name: "captionBlock",
+  group: "block",
+  content: "inline*",
+  defining: true,
+
+  parseHTML() {
+    return [{ tag: "div.caption-block" }];
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    return [
+      "div",
+      mergeAttributes(HTMLAttributes, { class: "caption-block" }),
+      0,
+    ];
+  },
+});
+
 export const FeatureImageBlockExtension = {
-  FeatureImageBlock: Node.create({
-    name: "featureImageBlock",
-    group: "block",
-    content: "altBlock captionBlock", // Define child nodes
-    draggable: true,
-
-    addAttributes() {
-      return {
-        imageUrl: { default: "" },
-        fullWidth: { default: true },
-      };
-    },
-
-    parseHTML() {
-      return [
-        {
-          tag: "feature-image-block",
-          getAttrs: (node) => {
-            const element = node as HTMLElement;
-            return {
-              imageUrl: element.getAttribute("data-image-url") || "",
-              fullWidth: element.getAttribute("data-full-width") !== "false",
-            };
-          },
-        },
-      ];
-    },
-
-    renderHTML({ HTMLAttributes }) {
-      return [
-        "feature-image-block",
-        mergeAttributes(HTMLAttributes, {
-          "data-image-url": HTMLAttributes.imageUrl,
-          "data-full-width": HTMLAttributes.fullWidth,
-        }),
-      ];
-    },
-
-    addNodeView() {
-      return ReactNodeViewRenderer(FeatureImageBlockNodeView, {
-        stopEvent: (event) => {
-          const target = event.event.target as HTMLElement;
-          return target.tagName === "INPUT" || target.tagName === "TEXTAREA";
-        },
-      });
-    },
-  }),
-
-  AltBlock: Node.create({
-    name: "altBlock",
-    group: "block",
-    content: "inline*",
-
-    parseHTML() {
-      return [{ tag: "div.alt-block" }];
-    },
-
-    renderHTML({ HTMLAttributes }) {
-      return [
-        "div",
-        mergeAttributes(HTMLAttributes, { class: "alt-block" }),
-        0,
-      ];
-    },
-  }),
-
-  CaptionBlock: Node.create({
-    name: "captionBlock",
-    group: "block",
-    content: "inline*",
-
-    parseHTML() {
-      return [{ tag: "div.caption-block" }];
-    },
-
-    renderHTML({ HTMLAttributes }) {
-      return [
-        "div",
-        mergeAttributes(HTMLAttributes, { class: "caption-block" }),
-        0,
-      ];
-    },
-  }),
+  FeatureImageBlock,
+  AltBlock,
+  CaptionBlock,
 };
